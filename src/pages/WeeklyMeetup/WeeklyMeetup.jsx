f6cace6a0edd524fa63ad1711fe8bdcce1beae11
Sophie Bonneval <sophie.bonneval@wearejh.com>
2023-12-08 20:52:50 +0000

    Add weekly meetup page

---
+++ src/pages/WeeklyMeetup/WeeklyMeetup.jsx
@@ -0,0 +1,14 @@
+import Page from '../../components/Page/Page';
+import Wrapper from '../../components/Wrapper/Wrapper';
+
+function WeeklyMeetup() {
+  return (
+    <Page>
+      <Wrapper>
+        <h1>Weekly Meetup</h1>
+      </Wrapper>
+    </Page>
+  );
+}
+
+export default WeeklyMeetup;
